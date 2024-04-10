49dda76969c7b78f7fc4fc732a645553485c32f1
Abhishek De <deabhishek06@gmail.com>
2024-04-10 16:30:40 +0530

    Connect mongoDB database.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const server = express();
 
+const mongoose = require("mongoose");
+
+main().catch((err) => {
+  console.error(err);
+});
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+  console.log("Connected to MongoDB");
+}
+
 server.get("/", (req, res) => {
   res.json({ status: "success" });
 });
