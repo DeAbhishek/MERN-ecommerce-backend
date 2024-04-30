e07046c31a76fcb48a48ea5f09808ae73a4d6981
Abhishek <deabhishek06@gmail.com>
2024-04-29 23:15:46 -0700

    Create UpdateCart and deleteCart controller and added to router.

--- controller/Cart.js
+++ controller/Cart.js
@@ -15,9 +15,29 @@ exports.addToCart = async (req, res) => {
   const cart = new Cart(req.body);
   try {
     const doc = await cart.save();
-    const result= await doc.populate("product");
+    const result = await doc.populate("product");
     res.status(201).json(result);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
 };
+
+exports.deleteFromCart = async (req, res) => {
+  try {
+    const doc = await Cart.findByIdAndDelete(req.params.id);
+    res.status(200).json(doc);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.updateCart = async (req,res)=>{
+  try {
+    const doc = await Cart.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(doc);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+}
--- routes/cart.js
+++ routes/cart.js
@@ -1,7 +1,16 @@
-const { addToCart, fetchCartByUser } = require("../controller/Cart");
+const {
+  addToCart,
+  fetchCartByUser,
+  deleteFromCart,
+  updateCart,
+} = require("../controller/Cart");
 
 const router = require("express").Router();
 
-router.post("/", addToCart).get("/", fetchCartByUser);
+router
+  .post("/", addToCart)
+  .get("/", fetchCartByUser)
+  .delete("/:id", deleteFromCart)
+  .patch("/:id", updateCart);
 
 module.exports = router;
