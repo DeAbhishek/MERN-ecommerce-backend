4f41d808b6140b86fa5bbd719e9ef28b6a5f03bd
Abhishek <deabhishek06@gmail.com>
2024-05-07 02:40:30 -0700

    Auth responses are set as per frontend requirements.

--- controller/Auth.js
+++ controller/Auth.js
@@ -3,8 +3,8 @@ const { User } = require("../model/User");
 exports.createUser = async (req, res) => {
   const user = new User(req.body);
   try {
-    await user.save();
-    res.status(201).json(user);
+    const doc = await user.save();
+    res.status(201).json({ id: doc.id, role: doc.role });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
@@ -19,9 +19,7 @@ exports.loginUser = async (req, res) => {
       : user.password === req.body.password
       ? res.status(200).json({
           id: user.id,
-          email: user.email,
-          name: user.name,
-          role:user.role,
+          role: user.role,
           message: "Login successful",
         })
       : res.status(400).json({ message: "Invalid email or password" });
