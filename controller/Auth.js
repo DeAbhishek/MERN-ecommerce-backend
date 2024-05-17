d0ae3696ba72516ee844a5c5c9dc6ee72845673f
Abhishek De <deabhishek06@gmail.com>
2024-05-18 02:03:35 +0530

    req.login used in createUser controller so that when users sign up, during which req.login() can be invoked to automatically log in the newly registered user and create a session.

--- controller/Auth.js
+++ controller/Auth.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
 const { User } = require("../model/User");
 const bcrypt = require("bcrypt");
-const { sanitizeUser } = require('../services/common');
-
+const { sanitizeUser } = require("../services/common");
 
 exports.createUser = (req, res) => {
   const newUserData = { ...req.body };
@@ -17,7 +16,13 @@ exports.createUser = (req, res) => {
         newUserData.password = hash;
         try {
           const doc = await User.create(newUserData);
-          res.status(201).json(sanitizeUser(doc));
+
+          req.login(sanitizeUser(doc), function (err) {
+            if (err) res.status(400).json({ message: err.message });
+            else {
+              res.status(201).json(sanitizeUser(doc));
+            }
+          });
         } catch (error) {
           res.status(400).json({ message: error.message });
         }
